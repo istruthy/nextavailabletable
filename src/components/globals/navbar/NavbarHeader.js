2a96bd4e99780423c9e48b21c9cfcb4eb0b49463
istruthy <daren.lunsford+istruthy@gmail.com>
2020-05-17 21:10:35 -0700

    (feat) building navbar

---
+++ src/components/globals/navbar/NavbarHeader.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react"
+import { Link } from "gatsby"
+import { FaBars } from "react-icons/fa"
+import { colors } from "../../../utils/styles"
+import styled from "styled-components"
+
+export default class NavbarHeader extends Component {
+  render() {
+    const { handleNavbar } = this.props
+
+    return (
+      <HeaderWrapper>
+        <Link to="/">Home</Link>
+        <FaBars
+          className="toggle-icon"
+          onClick={() => {
+            handleNavbar()
+          }}
+        ></FaBars>
+      </HeaderWrapper>
+    )
+  }
+}
+
+const HeaderWrapper = styled.div`
+  padding: 0.45rem 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  .toggle-icon {
+    font-size: 1.75rem;
+    color: ${colors.mainBlack};
+    cursor: pointer;
+  }
+
+  @media (min-width: 768px) {
+    .toggle-icon {
+      display: none;
+    }
+  }
+`
