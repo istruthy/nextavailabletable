6e8ad6eb092d99a0521b15b4008fc0e5cedf1cac
istruthy <daren.lunsford+istruthy@gmail.com>
2020-05-17 19:44:09 -0700

    (fix) removing boiler plate

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Next Available Table`,
+    description: `Helping you fill your tables with predictability and ease.`,
     author: `@gatsbyjs`,
   },
   plugins: [
